b156f590253159016f33c4e09387994440eedea3
p_craft <ef81sp@gmail.com>
2024-01-20 01:48:52 +0900

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,45 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    ...(VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'めんちんどーじょー2 | チンイツ練習アプリ',
+        short_name: 'めんちんどーじょー2',
+        description: '自分のペースでチンイツを練習できるWebアプリ"',
+        theme_color: '#d1fae5',
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }) as any),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-    dedupe: [
-      'vue'
-    ]
+    dedupe: ['vue'],
   },
-  define: { 
-    'import.meta.vitest': 'undefined', 
-  }, 
+  define: {
+    'import.meta.vitest': 'undefined',
+  },
 })
