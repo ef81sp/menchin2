5be499c1e31d783fd0f288bb63948e930ea99860
p_craft <ef81sp@gmail.com>
2024-01-27 12:46:39 +0900

    pairiを更新

--- src/utils/generateHandUtils.ts
+++ src/utils/generateHandUtils.ts
@@ -47,12 +47,3 @@ export const filterRange = <T extends Manzu[] | Pinzu[] | Sozu[]>(
       ) as T
   }
 }
-
-export const randomlyNotenOrTempai = (): 'noten' | 'tempai' => {
-  const random = Math.random()
-  if (random < 0.5) {
-    return 'noten'
-  } else {
-    return 'tempai'
-  }
-}
