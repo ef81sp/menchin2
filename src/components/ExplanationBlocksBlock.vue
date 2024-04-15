d7878c49b9299287476830e90f3b747b28c9bea8
p_craft <ef81sp@gmail.com>
2024-04-15 23:02:44 +0900

    説明を1行で表示するようにした

--- src/components/ExplanationBlocksBlock.vue
+++ src/components/ExplanationBlocksBlock.vue
@@ -1,26 +1,14 @@
 <script setup lang="ts">
-import type { PaiStr } from '@/composables/PaiStr.type'
 import VPai from './VPai.vue'
-import type { ExtractResult5ブロック } from '@/utils/type'
+import type { StrPai } from '@/utils/type'
 defineProps<{
   title: string
-  blockList: Block[]
+  blockList: StrPai[][]
 }>()
-
-type Block = Janto | Mentsu | Tatsu
-type Janto = ExtractResult5ブロック['雀頭']
-type Mentsu = ExtractResult5ブロック['面子'][number]
-type Tatsu = ExtractResult5ブロック['塔子'][number]
-const blockToPaiStrList = (block: Janto | Mentsu | Tatsu): PaiStr[] => {
-  if (block === null) {
-    return []
-  }
-  return block.component.map((p) => p.toString())
-}
 </script>
 <template>
   <div class="flex gap-x-2 lg:gap-x-4">
-    <p class="flex min-w-fit items-center text-sm md:text-base lg:text-lg">{{ title }}</p>
+    <!-- <p class="flex min-w-fit items-center text-sm md:text-base lg:text-lg">{{ title }}</p> -->
     <div class="flex flex-wrap items-center gap-2 lg:gap-4">
       <div
         class="flex"
@@ -28,7 +16,7 @@ const blockToPaiStrList = (block: Janto | Mentsu | Tatsu): PaiStr[] => {
         :key="i"
       >
         <VPai
-          v-for="(p, i) in blockToPaiStrList(paiStrList)"
+          v-for="(p, i) in paiStrList"
           :pai-str="p"
           :key="i"
           class="w-5 md:w-8 lg:w-11"
--- src/components/NanimachiExplanationDialog.vue
+++ src/components/NanimachiExplanationDialog.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 
-import type { AnalysisResult13 } from '@/utils/type'
+import type { AnalysisResult13, NanimachiExplanation } from '@/utils/type'
 
 import Dialog from 'primevue/dialog'
 import ExplanationBlocks from './ExplanationBlocks.vue'
@@ -10,7 +10,6 @@ import ExplanationSevenPairs from './ExplanationSevenPairs.vue'
 
 import { visibleNanimachiExplanation } from '@/composables/dialogController'
 import { hand } from '@/composables/nanimachiHand'
-import type { 牌 } from 'pairi'
 
 const props = defineProps<{ analysisResult: AnalysisResult13 }>()
 
@@ -20,6 +19,38 @@ const shanten = computed(() =>
     : props.analysisResult.シャンテン数 + 'シャンテン',
 )
 const yuukoText = computed(() => (props.analysisResult.シャンテン数 === 0 ? '待ち' : '有効牌'))
+
+const indivisualsStr = computed<NanimachiExplanation[]>(() => {
+  const wholeResult = props.analysisResult.analysisResult._5ブロック.indivisuals.map(
+    ({ ブロック, 有効牌 }) => {
+      const yuuko = 有効牌.map((p) => p.toString())
+
+      const _block = [ブロック.雀頭 ?? [], ...ブロック.面子, ...ブロック.塔子]
+        .flat()
+        .map((block) => {
+          return block.component.map((p) => p.toString())
+        })
+
+      const _restList = Object.values(ブロック.rest).flat()
+      const _rest = _restList.map((p) => [p.toString()])
+      const block = [..._block, ..._rest].sort()
+      return { yuuko, block }
+    },
+  )
+
+  // シャンポン待ちが冗長なので、blockが同一のものをひとつにまとめ、yuukoはその全てをマージする
+  const unionSame = wholeResult.reduce<NanimachiExplanation[]>((acc, cur) => {
+    const index = acc.findIndex((a) => JSON.stringify(a.block) === JSON.stringify(cur.block))
+    if (index === -1) {
+      return [...acc, cur]
+    }
+    // yuukoはユニークにする
+    acc[index].yuuko = Array.from(new Set([...acc[index].yuuko, ...cur.yuuko])).sort()
+
+    return acc
+  }, [])
+  return unionSame
+})
 </script>
 <template>
   <Dialog
@@ -33,13 +64,12 @@ const yuukoText = computed(() => (props.analysisResult.シャンテン数 === 0 
     <h4 class="my-4 md:text-2xl">{{ shanten }}</h4>
     <ExplanationBlocksSinglePaiList
       :title="yuukoText"
-      :pai-list="props.analysisResult.有効牌"
+      :pai-list="props.analysisResult.有効牌.map((p) => p.toString())"
     />
 
-    <!-- 型が合わん。 -->
     <ExplanationBlocksSinglePaiList
       title="問題"
-      :pai-list="hand.普通"
+      :pai-list="hand.普通.map((p) => p.toString())"
       class="mt-12"
     />
 
@@ -51,8 +81,9 @@ const yuukoText = computed(() => (props.analysisResult.シャンテン数 === 0 
     >
       <h5 class="mt-16 md:text-xl">5ブロック</h5>
       <ExplanationBlocks
-        v-for="(indivisual, i) in props.analysisResult.analysisResult._5ブロック.indivisuals"
-        :indivisual="indivisual"
+        v-for="(indivisual, i) in indivisualsStr"
+        :nanimachi-explanation="indivisual"
+        :shanten="props.analysisResult.シャンテン数"
         :key="i"
       />
     </template>
