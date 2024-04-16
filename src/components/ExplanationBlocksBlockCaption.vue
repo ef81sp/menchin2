6ce74c95c149adc4e5b3456449f272570cc5e3f1
p_craft <ef81sp@gmail.com>
2024-04-16 13:21:50 +0900

    色を調整

--- src/components/ExplanationBlocksBlockCaption.vue
+++ src/components/ExplanationBlocksBlockCaption.vue
@@ -4,29 +4,29 @@ const props = defineProps<{
   type: '面子' | '搭子' | '対子' | '雀頭' | '単騎' | '雀頭候補' | 'くっつき' | '不要'
 }>()
 
-const color = computed<string>(() => {
+const gradientColorFrom = computed<string>(() => {
   switch (props.type) {
     case '面子':
     case '雀頭':
-      return 'green-500'
+      return 'from-green-400'
     case '搭子':
     case '対子':
     case '単騎':
-      return 'yellow-500'
+      return 'from-amber-500'
     case 'くっつき':
     case '雀頭候補':
-      return 'blue-500'
+      return 'from-blue-500'
     case '不要':
-      return 'gray-500'
+      return 'from-gray-400'
     default:
-      return 'white'
+      throw new Error('Invalid type')
   }
 })
 </script>
 <template>
   <div
-    class="flex justify-center pt-1"
-    :class="`bg-gradient-to-b from-${color} to-40%`"
+    class="flex justify-center bg-gradient-to-b to-40% pt-1"
+    :class="gradientColorFrom"
   >
     <p
       class="max-w-8 text-nowrap object-fill text-center text-xs md:text-sm"
